bbefb790108048827c9a346f8efd6ef62954e7f2
Erik <37380305+erikrosdorff@users.noreply.github.com>
2022-03-10 19:19:06 +0300

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,30 @@
+adjective = input("Enter an Adjective: ")
+adjective2 = input("Enter another Adjective: ")
+type_of_bird = input("Enter a type of a bird: ")
+room = input("Enter a room in the house ")
+second_form = input("Enter the second form of a verb: ")
+base_form = input("Enter the base form of a verb: ")
+relative = input("Enter the name of a relative: ")
+noun = input("Enter a noun: ")
+liquid = input("Enter a liquid: ")
+verb_ing_form = input("Enter a verb in 'ing' form: ")
+part_of_body = input("Enter a part of the body: ")
+plural_noun = input("Enter a plural noun: ")
+verb_ing_form2 = input ("Enter a verb in 'ing' form: ")
+noun2 = input ("Enter a noun: ")
+
+
+madlib = f"""It was a __{adjective}__, cold November day. \
+I woke up to the __{adjective2}__ smell of __{type_of_bird}__ roasting in the __{room}__ downstairs.\
+ I __{second_form}__ down the stairs to see if I could help __{base_form}__ the dinner. \
+  My mom said, 'See if __{relative}__ needs a fresh __{noun}__' \
+  So I carried a tray of glasses full of __{liquid}__ inot the __{verb_ing_form}__ room.\
+  When I got there, I couldn't believe my __{part_of_body}__!\
+  There were __{plural_noun}___ ___{verb_ing_form2}__ on the __{noun2}__!"""
+
+print(madlib)
+
+
 # The following are examples of how to input text:
 # from contextlib import nullcontext
 # shepherd = "Mary"
@@ -13,28 +40,3 @@
 # print(f"Here is some text -> {some_text}")
 
 #Using different classes to insert into the code. Idealily "Fill in the gap"
-
-adjective = input("Enter an Adjective: ")
-adjective2 = input("Enter another Adjective: ")
-type_of_bird = input("Enter a type of a bird: ")
-room = input("Enter a room in the house ")
-second_form = input("Enter the second form of a verb: ")
-base_form = input("Enter the base form of a verb: ")
-relative = input("Enter the name of a relative: ")
-liquid = input("Enter a liquid: ")
-verb_ing_form = input("Enter a verb in 'ing' form: ")
-part_of_body = input("Enter a part of the body")
-plural_noun = input("Enter a plural noun: ")
-verb_ing_form2 = input ("Enter a verb in 'ing' form: ")
-noun = input ("Enter a noun: ")
-
-
-madlib = f"""It was a {adjective}, cold November day.  \
-I woke up to the {adjective2} smell of {type_of_bird} roasting in the {room} downstairs. \
- I {second_form} down the stairs to see if I could help {base_form} the dinner. \
-  My mom said, 'See if {relative} needs a fresh __noun__'  \
-  So I carried a tray of glasses full of {liquid} inot the ___{verb_ing_form}___ room. \
-  When I got there, I couldn't believe my ___{part_of_body}___! \
-  There were {plural_noun} {verb_ing_form2} on the {noun}!"""
-
-print(madlib)
